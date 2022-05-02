a2d135c55d2d571b2124fe08a9d510a5eb4fbf76
amijuri <miju1121@outlook.jp>
2022-05-02 13:24:05 +0900

    baladeの24行目修正

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -21,7 +21,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <!-- Styles -->
-        <link href="{{ asset('css/member.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     </head>
     <body>
     

e9f4037140bc291f60a0f92ed2eeba54ea0220e4
taichi <taichi.hamada514@gmail.com>
2022-05-22 23:37:12 +0900

    2022/05/22 確認画面・編集画面更新

--- resources/views/event/registerconfirm.blade.php
+++ resources/views/event/registerconfirm.blade.php
@@ -29,35 +29,36 @@
 </div>
 
 <div style="width: 400px; text-align:left; margin: 10px auto;">
-    <form action="/event" method="get">
+    <form action="/event/eventRegister" method="post">
         @csrf
         <div class="form-group">
-            イベント名：{{$event[event_name]}}
-            <input class="form-control" value="{{$event[event_name]}}" type="hidden" name="event_name">
-            イベント種別：{{$event[event_category]}}
-            <input class="form-control" value="{{$event[event_category]}}" type="hidden" name="event_category">       
-            イベント詳細：{{$event[overview]}}
-            <input class="form-control" value="{{$event[overview]}}" type="hidden" name="overview">           
-            開催日時：{{$event[event_date]}}
-            <input class="form-control" value="{{$event[event_date]}}" type="hidden" name="event_date">
-            開催場所：{{$event[place]}}
-            <input class="form-control" value="{{$event[place]}}" type="hidden" name="place">
-            参加料金：{{$event[price]}}
-            <input class="form-control" value="{{$event[price]}}" type="hidden" name="price">
-            申込開始日：{{$event[period_start]}}
-            <input class="form-control" value="{{$event[period_start]}}" type="hidden" name="period_start">
-            申込終了日：{{$event[period_end]}}
-            <input class="form-control" value="{{$event[period_end]}}" type="hidden" name="period_end">
+            <p>イベント名：{{$event['event_name']}}</p>
+            <input class="form-control" value="{{$event['event_name']}}" type="hidden" name="event_name">
+            <p>イベント種別：{{$event['event_category']}}</p>
+            <input class="form-control" value="{{$event['event_category']}}" type="hidden" name="event_category">       
+            <p>イベント詳細：{{$event['overview']}}</p>
+            <input class="form-control" value="{{$event['overview']}}" type="hidden" name="overview">           
+            <p>開催日時：{{$event['event_date']}}</p>
+            <input class="form-control" value="{{$event['event_date']}}" type="hidden" name="event_date">
+            <p>開催場所：{{$event['place']}}</p>
+            <input class="form-control" value="{{$event['place']}}" type="hidden" name="place">
+            <p>参加料金：{{$event['price']}}</p>
+            <input class="form-control" value="{{$event['price']}}" type="hidden" name="price">
+            <p>申込開始日：{{$event['period_start']}}</p>
+            <input class="form-control" value="{{$event['period_start']}}" type="hidden" name="period_start">
+            <p>申込終了日：{{$event['period_end']}}</p>
+            <input class="form-control" value="{{$event['period_end']}}" type="hidden" name="period_end">
             <br>
-            <p>{{$event[status]}}</p>
-            <input class="form-control" value="{{$event[status]}}" type="hidden" name="status">
+            <p>状態：{{$event['status']}}</p>
+            <input class="form-control" value="{{$event['status']}}" type="hidden" name="status">
             <br>
             <p></p>
-            備考欄：{{$event[remarks]}}
-            <input class="form-control" value="{{$event[remarks]}}" type="text" name="remarks">
+            <p>備考欄：{{$event['remarks']}}</p>
+            <input class="form-control" value="{{$event['remarks']}}" type="hidden" name="remarks">
+
             <button type="submit" class="btn btn-secondary">登録</button>
             <div class="form-group">
-                <a href=""><button type="button" class="btn btn-secondary">戻る</button></a>
+                <a href="/event/register"><button type="button" class="btn btn-secondary">戻る</button></a>
             </div>
         </div>
     </form>

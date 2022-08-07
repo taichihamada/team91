e54e3271a473b1ea31b4b39fcf3f7191ca83c853
hino-a <hino06170526@outlook.jp>
2022-08-07 12:40:13 +0900

    20220807_12-40

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment(['production'])) {
             \URL::forceScheme('https');
         }
+
+        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
+        
     }
 }
